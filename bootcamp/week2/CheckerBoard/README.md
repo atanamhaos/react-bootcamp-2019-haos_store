c0094384b2f56d621d85cd82e3c81570e62ae420
Ben Haos <benjaminhaos@northwesternmutual.com>
2019-03-13 03:38:15 -0500

    Sink!

---
+++ bootcamp/week2/CheckerBoard/README.md
@@ -0,0 +1,4 @@
+browserdump
+
+* [Learn React.js: Ridiculously Simple Forms](http://jamesknelson.com/learn-raw-react-ridiculously-simple-forms/)
+s
---
+++ bootcamp/week2/Counter/index.js
@@ -0,0 +1,27 @@
+
+
+console.log('Hi');
+
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Stateful component!'
+        }
+    }
+    
+    
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.text}</h1>
+                <button
+                    onClick={() => this.setState({ text: 'State has changed!' })} >
+                    Click to change state's text value!
+                </button>
+            </div>
+        )
+    }
+}
