89de1a6a707f1b8b0cf0993c4510ec76064d4349
Benjamin Haos <benjaminhaos@gmail.com>
2019-03-15 08:00:23 +0000

    Add notes from updating ide vm to work w. create-react-app.

---
+++ ToDo/important/setup_c9_4_react_bootcamp.bash
@@ -0,0 +1,23 @@
+## Setup C9 for React Bootcamp!
+
+### Step 1
+
+#'''
+#sudo apt-get update
+#sudo apt-get install -y build-essential checkinstall libssl-dev
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+# export NVM_DIR="/home/ubuntu/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+### Step 2
+
+#'''
+# nvm install 10.15.3
+# nvm use 10.15.3
+# nvm alias default 10.15.3
+#'''
+
+#
+#npm install -g create-react-app
+
+#create-react-app my-app
