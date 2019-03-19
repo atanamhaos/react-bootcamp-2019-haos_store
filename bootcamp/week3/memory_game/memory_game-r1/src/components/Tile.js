630038e0b22d4bbbd3b4fec858bb9e889d7957f3
Benjamin Haos <benjaminhaos@gmail.com>
2019-03-19 19:49:53 +0000

    Testing build

--- bootcamp/week3/memory_game/memory_game-r1/src/components/Tile.js
+++ bootcamp/week3/memory_game/memory_game-r1/src/components/Tile.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import '../styles/tile.css';
 
-
-    
 const Tile = (props) => {
-
 
     return (
        <button className='tile' id={`${props.rowIndex}${props.tileIndex}`} value={props.valueOfTile} onClick={(event)=>{props.tileClickEvent(event)}}></button> 
