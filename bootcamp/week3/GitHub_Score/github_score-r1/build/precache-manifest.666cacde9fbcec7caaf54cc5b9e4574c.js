0e5628b391aef4846294fb6d2ab5294dac24b630
Benjamin Haos <benjaminhaos@gmail.com>
2019-03-22 12:07:22 +0000

    setup for pages delivery.

---
+++ bootcamp/week3/GitHub_Score/github_score-r1/build/precache-manifest.666cacde9fbcec7caaf54cc5b9e4574c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16bd8e53cf98234f9eb8",
+    "url": "/static/js/main.e11a4b99.chunk.js"
+  },
+  {
+    "revision": "2f0cb50dce2f2d019afd",
+    "url": "/static/js/2.93921897.chunk.js"
+  },
+  {
+    "revision": "d25843642580b814102c8c4f19f4aee6",
+    "url": "/index.html"
+  }
+];
