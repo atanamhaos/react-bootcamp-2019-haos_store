a679ca6e7f539ff6d620b1f3182bcce19d2d76eb
Benjamin Haos <benjaminhaos@gmail.com>
2019-04-05 11:39:59 +0000

    Add tab selection connected to store and working tab add functionality.

--- bootcamp/week5/Dojo_Dossier/dojo_dossier-r1/src/components/redux.js
+++ bootcamp/week5/Dojo_Dossier/dojo_dossier-r1/src/components/redux.js
@@ -3,15 +3,22 @@ import { createStore } from 'redux';
 export const updateAddTabField = (fieldValue) => ({
     type: 'UPDATE_TAB_ADD_FIELD',
     fieldValue,
-})
+});
 
 export const addTab = () => {
     return ({
     type: 'ADD_TAB',
 })};
 
+export const selectTab = ({id}) => {
+    //console.log('selectTab(...) selectedTabsId : ',id);
+    return ({
+    type: 'SELECT_TAB',
+    selectedTabsId:id,
+})};
+
 export const reducers = (state = initialState, action) => {
-    console.log('reducers!');
+    //console.log('reducers!');
     switch (action.type) {
 
         case 'UPDATE_TAB_ADD_FIELD':
@@ -21,12 +28,23 @@ export const reducers = (state = initialState, action) => {
             return updatedStatesAddTabField;
 
         case 'ADD_TAB':
-            console.log('dispatch ADD_TAB');
-            console.log(state);
-            
-            return state;
+            //console.log('dispatch ADD_TAB');
+            //console.log(state);
+            let addTabReducersNewState = {...state};
+            let nextId = addTabReducersNewState.persons.length + 1;
+            let newPerson = {id:nextId, name:addTabReducersNewState.addTabField, listdata:{}};
+            addTabReducersNewState.persons.push(newPerson);
+            addTabReducersNewState.addTabField = '';
+            return addTabReducersNewState;
 
+        case 'SELECT_TAB':
+            //console.log('SELECT_TAB, selectedTabsId : ',action.selectedTabsId);
+            let selectTabsNewState = {...state};
+            selectTabsNewState.selectedTab = action.selectedTabsId;
+            return selectTabsNewState;
+        
         default:
+            console.log('hit default!');
             return state;
     }
 };
@@ -34,20 +52,20 @@ export const reducers = (state = initialState, action) => {
 // Define Initial State 
 const initialState = {
     persons: [
-        { id: '001', name: 'Melissa', listdata: {}, },
-        { id: '002', name: 'Yoseph', listdata: {}, },
-        { id: '003', name: 'Jeremy', listdata: {}, },
-        { id: '004', name: 'Jeff', listdata: {}, },
-        { id: '005', name: 'Caleb', listdata: {}, },
-        { id: '006', name: 'Zachary', listdata: {}, },
-        { id: '007', name: 'Swapna', listdata: {}, },
-        { id: '008', name: 'Durga', listdata: {}, },
-        { id: '009', name: 'Benjamin', listdata: {}, },
-        { id: '010', name: 'Speros', listdata: {}, },
+        { id: '001', name: 'Melissa', listdata: ['feline husbandry'], },
+        { id: '002', name: 'Yoseph', listdata: [], },
+        { id: '003', name: 'Jeremy', listdata: ['testing code'], },
+        { id: '004', name: 'Jeff', listdata: ['child rearing', ' gameing', 'foodie'], },
+        { id: '005', name: 'Caleb', listdata: ['feline husbandry', ' inovation', ' comedy'], },
+        { id: '006', name: 'Zachary', listdata: ['baseball', ' frugality'], },
+        { id: '007', name: 'Swapna', listdata: ['child rearing'], },
+        { id: '008', name: 'Durga', listdata: ['child rearing, child creation, electronics'], },
+        { id: '009', name: 'Benjamin', listdata: ['creation', 'electronics', 'hacking',], },
+        { id: '010', name: 'Speros', listdata: ['teaching', 'coding', 'entrepreneurship', 'hiking',], },
     ],
     addTabField: '',
+    selectedTab:'000',
 };
-
 // STORE
 export function configureStore(initialState) {
     const store = createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
--- bootcamp/week5/Dojo_Dossier/dojo_dossier-r1/src/components/tabs.js
+++ bootcamp/week5/Dojo_Dossier/dojo_dossier-r1/src/components/tabs.js
@@ -4,57 +4,10 @@ import { Link } from "react-router-dom";
 
 import { addTab } from "./redux";
 import { updateAddTabField } from "./redux";
+import { selectTab } from "./redux";
 
-const Tabs = ({ persons, addTab, newtabfield, updateAddTabField }) => {
-
-    function test() {
-        console.log('test runs');
-        // Get the modal
-        var modal = document.getElementById('myModal');
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("addtabtab");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementById("modelclose");
-        //document.getElementById("modelclose")
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target === modal) {
-                modal.style.display = "none";
-            }
-        }
-        addTab();
-    }
-    const addtabtab = () => { return (<div><Link to={'/'} id='addtabtab' style={addtabtabstyle()} onClick={() => test()}>&nbsp;&nbsp;&nbsp;+</Link></div>) };
-
-    const addtabtabstyle = () => {
-        return {
-            float: 'right',
-            border: '3px 3px 3px 30px',
-            borderStyle: 'solid solid solid solid',
-            borderWidth: '10px 1px 1px 1px',
-            borderColor: '#004c44 #093c8e #1759c4 #1759c4',
-            fontWeight: 'bold',
-            borderRadius: '50px 15px 0px 0px',
-
-            margin: '0px 2px 0px 0px',
-            padding: '5px 20px 0px 0px',
-            textDecoration: 'none',
-
-            fontSize: '1.5em',
-        };
-    }
-
+const Tabs = ({ persons, addTab, newtabfield, updateAddTabField, selectTab }) => {
+    // The Tabs Style
     const tabStyle = (numtabs) => {
         return {
             float: 'left',
@@ -71,13 +24,44 @@ const Tabs = ({ persons, addTab, newtabfield, updateAddTabField }) => {
 
             fontSize: '1.5em',
         };
-    }
-
+    };
+    // The Tabs
     const tabs = persons.map((person, index, array) => {
-
-        return <Link to={'/' + person.id} style={tabStyle(array.length)} key={person.id}>{person.name}</Link>;
+        return <Link 
+              to={'/' + person.id} 
+              style={tabStyle(array.length)} 
+              key={person.id} 
+              onClick={(event) => { 
+                //console.log(selectTab);
+                event.preventDefault();
+                selectTab(person);}
+                  
+              }
+            >{person.name}</Link>;
     });
 
+    // Add Tab Tab
+    const addtabtab = () => {
+        return (<div><Link to={'/'} id='addtabtab' style={addtabtabstyle()} onClick={() => activatemodal()}>&nbsp;&nbsp;&nbsp;+</Link></div>);
+    };
+    // Add Tab Tab Style
+    const addtabtabstyle = () => {
+        return {
+            float: 'right',
+            border: '3px 3px 3px 30px',
+            borderStyle: 'solid solid solid solid',
+            borderWidth: '10px 1px 1px 1px',
+            borderColor: '#004c44 #093c8e #1759c4 #1759c4',
+            fontWeight: 'bold',
+            borderRadius: '50px 15px 0px 0px',
+
+            margin: '0px 2px 0px 0px',
+            padding: '5px 20px 0px 0px',
+            textDecoration: 'none',
+
+            fontSize: '1.5em',
+        };
+    };
     /* The Modal (background) */
     const modalstyle = {
         display: 'none', // Hidden by default 
@@ -91,62 +75,84 @@ const Tabs = ({ persons, addTab, newtabfield, updateAddTabField }) => {
         overflow: 'auto', // Enable scroll if needed 
         backgroundColor: 'rgb(0,0,0)', // Fallback color
         backgroundColor: 'rgba(0,0,0,0.4)', // Black w/ opacity
-    }
-
-
-    /* Modal Content */
+    };
+    /* Modal Content Style*/
     const modalcontentstyle = {
         backgroundcolor: '#fefefe',
         margin: 'auto',
         padding: '20px',
         border: '1px solid #888',
         width: '80%',
-    }
+    };
+    /* Modal Close Style*/
     const closestyle = {
         color: '#aaaaaa',
         float: 'right',
         fontSize: '28px',
         fontWeight: 'bold',
+    };
+    // This bit makes the model work, if you want to call it that...
+    function activatemodal() {
+        // This, and the model element below were grabbed from some forgotten tutorial and made/hacked to work...
+        // Get the modal
+        var modal = document.getElementById('myModal');
+        // Get the button that opens the modal
+        var btn = document.getElementById("addtabtab");
+        // Get the <span> element that closes the modal
+        var span = document.getElementById("modelclose");
+        modal.style.display = "block";
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() { modal.style.display = "block"; };
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { modal.style.display = "none"; };
+        // When the user clicks anywhere outside of the modal, close it
+        // ToDo: Turn this into a ternary
+        window.onclick = function(event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        };
     }
+
+
 
     return (
         <div className="navtabs">
           {tabs}
+          {/*<!-- The Modal -->*/}
+          <div id="myModal" style={modalstyle}>
+            {/*<!-- Modal content -->*/}
+            <div style={modalcontentstyle}>
+              <span id="modelclose" style={closestyle}>&times;</span>
+              <p>New Tab:</p>
+              <form onSubmit={(event) =>{
+                  event.preventDefault();
+                  addTab();
+                  //var span = document.getElementById("modelclose");
+                  var modal = document.getElementById('myModal');
+                  modal.style.display = "none";
+                  }}>
+                <input value={newtabfield} onChange={(event) => {updateAddTabField(event.target.value)}}/>
+              </form>
+            </div>
+          </div>
           {addtabtab()}
-
-{/*<!-- The Modal -->*/}
-<div id="myModal" style={modalstyle}>
-
-  {/*<!-- Modal content -->*/}
-  <div style={modalcontentstyle}>
-    <span id="modelclose" style={closestyle}>&times;</span>
-    <p>New Tab:</p>
-       <form onSubmit={(event) =>{
-       event.preventDefault();
-       addTab();
-       //var span = document.getElementById("modelclose");
-       var modal = document.getElementById('myModal');
-       modal.style.display = "none";
-       }}>
-       <input value={newtabfield} onChange={(event) => {updateAddTabField(event.target.value)}}/>
-       </form>
-
-  </div>
-
-</div>
-      
-      </div>
+        </div>
     );
 };
+
+
 
 const mapStateToProps = (state) => ({
     persons: state.persons,
     newtabfield: state.addTabField,
+    selectedTab: state.selectedTab,
 });
 
 const mapDispatchToProps = (dispatch) => ({
     updateAddTabField: (fieldValue) => dispatch(updateAddTabField(fieldValue)),
     addTab: (newTabToAdd) => dispatch(addTab(newTabToAdd)),
+    selectTab: (event) => dispatch(selectTab(event)),
 });
 
 export default connect(
