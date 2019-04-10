50afff6dabc912410bb6c52e753a872000070015
Benjamin Haos <benjaminhaos@gmail.com>
2019-04-10 15:13:36 +0000

    Commit of scripted setup bits.

--- z_scrpys/setup_c9_vm_step1.bash
+++ z_scrpys/setup_c9_vm_step1.bash
@@ -1,7 +1,11 @@
+ 
+ 
 sudo apt-get update
 sudo apt-get -y install build-essential checkinstall libssl-dev
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 command -v nvm
+
+# ToDo: Console out what is happening and direction for finishing the setup
 # ToDo: Setup pause and then close of shell
-# ToDo: Console out what is happening
 # ToDo: put output to logs
+# ToDo: Add in the auto correcting/quiting shebang
--- z_scrpys/setup_c9_vm_step2.bash
+++ z_scrpys/setup_c9_vm_step2.bash
@@ -1,44 +1,22 @@
-#!/bin/bash
-# 
 
 
-ToDo: Make this work!
-
-
-: <<'END' # Block Comment STARTS
-
+# This sources NVM for the script.
+. $NVM_DIR/nvm.sh
 # Install more current version of node
 nvm install 10.15.3
 # Make sure it is being used
 nvm use 10.15.3
 # Set it as default
 nvm alias default node
+
 # Install nodemon globally for use with express server
 npm install -g nodemon
 
-# CLI history START
-git clone git@github.com:atanamhaos/react-bootcamp-2019-haos_store.git
-ls
-bash react-bootcamp-2019-haos_store/z_scrpys/setup_c9_vm_step1.bash 
-exit
-node -v
-exit
-bash react-bootcamp-2019-haos_store/z_scrpys/setup_c9_vm_step2
-bash react-bootcamp-2019-haos_store/z_scrpys/setup_c9_vm_step2.bash 
-nvm -v
-bash react-bootcamp-2019-haos_store/z_scrpys/setup_c9_vm_step2.bash 
-nvm install 10.15.3
-nvm use 10.15.3'
-nvm use 10.15.3
-nvm alias default node
-npm install -g nodemon
-# This did not setup quickly, (Further setup instruction exists...)
-npm i -g create-react-redux-app-structure
-create-react-redux-app-structure my-app
-cd my-app/
-npm run fast-start
-npm start
-history
-# CLI history END
+
+: <<'END' # Block Comment STARTS
 END
 # Block Comment ENDS
+
+# ToDo: Console out what is happening and direction for using the setup
+# ToDo: put output to logs
+# ToDo: Add in the auto correcting/quiting shebang
