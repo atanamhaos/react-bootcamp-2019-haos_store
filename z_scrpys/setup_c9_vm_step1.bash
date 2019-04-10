7ed0fce8c6f2f1271f0c911570f7584a9353014f
Benjamin Haos <benjaminhaos@gmail.com>
2019-04-10 14:42:31 +0000

    Commit B4 burn of exam study box.

---
+++ z_scrpys/setup_c9_vm_step1.bash
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get -y install build-essential checkinstall libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+command -v nvm
+# ToDo: Setup pause and then close of shell
+# ToDo: Console out what is happening
+# ToDo: put output to logs
